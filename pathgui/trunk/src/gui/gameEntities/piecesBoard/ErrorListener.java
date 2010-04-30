dc887d6e7b7d9c974203a3dcf442521960f1bb2a
lucass <lucass@ce03f93c-135a-11de-bcda-2bb9902f8029>
2010-04-30 02:02:24 +0000

    Maven folder structure 2
    git-svn-id: http://pathboardgame.googlecode.com/svn/trunk@85 ce03f93c-135a-11de-bcda-2bb9902f8029

--- pathgui/trunk/src/gui/gameEntities/piecesBoard/ErrorListener.java
+++
@@ -1,5 +0,0 @@
-package gui.gameEntities.piecesBoard;
-
-public interface ErrorListener {
-	public void error(final String errorMessage);
-}
--- pathgui/trunk/src/gui/gameEntities/piecesBoard/PlayListener.java
+++
@@ -1,7 +0,0 @@
-package gui.gameEntities.piecesBoard;
-
-public interface PlayListener {
-
-	public void played();
-
-}
